8642c6231cc98130c777ef76eaef7fda0dfe1a04
feroroth <jakubfrantisekroth@gmail.com>
2026-03-03 18:01:08 +0100

    Added README.md, changed names of functions and added some #

---
+++ README.md
@@ -0,0 +1,7 @@
+#Simple calculator in python using **tkinter** library
+
+I used **tkinter** to build a GUI for my calculator. It's a very simple calculator and my first project. 
+
+- it uses eval for calculations
+- it also uses try and except for **ZeroDivisionError**
+- GUI has only buttons and label, "screen" of calculator, output is in terminal and visible **only after "=" button is pressed**
--- calculator.py
+++ calculator.py
@@ -4,6 +4,9 @@ root = tk.Tk()
 
 function = []
 
+# functions c0-9 are for numbers, each prints number and append it to list 
+# list is used for later calculation
+# named c after cislo 
 def c0():
     print("0", end = "")
     function.append('0') 
@@ -44,6 +47,7 @@ def c9():
     print("9", end = "")
     function.append('9')
 
+# functions for plus, minus,... work same as numbers
 def plus():
     print (" + ", end = "")
     function.append("+")
@@ -60,50 +64,55 @@ def div():
     print (" \u00f7 ", end = "")
     function.append("/")
 
-def equals():
-    print (f' = {eval(''.join(function))}')
-    
-def c():
+def dot():
     print (".", end = "")
     function.append(".")
-  
 
+# do actual calculation using eval command, it breaks list into string and evaluates it
+def equals():
+    try:
+        print (f" = {eval(''.join(function))}")
+    except ZeroDivisionError:
+        print (" = Syntax error")
+  
+# creating buttons, each section is different row
 button1 = tk.Button(root, text = "  1  ", command = c1)
 button2 = tk.Button(root, text = "  2  ", command = c2)
 button3 = tk.Button(root, text = "  3  ", command = c3)
-tplus = tk.Button(root, text = "  +  ", command = plus)
-tmult = tk.Button(root, text = "  x  ", command = mult)
+plus_button = tk.Button(root, text = "  +  ", command = plus)
+mult_button = tk.Button(root, text = "  x  ", command = mult)
 button1.grid(row = 1, column = 0)
 button2.grid(row = 1, column = 1)
 button3.grid(row = 1, column = 2)
-tplus.grid(row = 1, column = 3)
-tmult.grid(row = 1, column = 4)
+plus_button.grid(row = 1, column = 3)
+mult_button.grid(row = 1, column = 4)
 
 button4 = tk.Button(root, text = "  4  ", command = c4)
 button5 = tk.Button(root, text = "  5  ", command = c5)
 button6 = tk.Button(root, text = "  6  ", command = c6)
-tminus = tk.Button(root, text = "  -  ", command = minus)
-tdiv = tk.Button(root, text = " \u00f7 ", command = div)
+minus_button = tk.Button(root, text = "  -  ", command = minus)
+div_button = tk.Button(root, text = " \u00f7 ", command = div)
 button4.grid(row = 2, column = 0)
 button5.grid(row = 2, column = 1)
 button6.grid(row = 2, column = 2)
-tminus.grid(row = 2, column = 3)
-tdiv.grid(row = 2, column = 4)
+minus_button.grid(row = 2, column = 3)
+div_button.grid(row = 2, column = 4)
 
 button7 = tk.Button(root, text = "  7  ", command = c7)
 button8 = tk.Button(root, text = "  8  ", command = c8)
 button9 = tk.Button(root, text = "  9  ", command = c9)
-tequals = tk.Button(root, text = " = ", command = equals)
+equals_button = tk.Button(root, text = " = ", command = equals)
 button7.grid(row = 3, column = 0)
 button8.grid(row = 3, column = 1)
 button9.grid(row = 3, column = 2)
-tequals.grid(row = 3, column = 3)
+equals_button.grid(row = 3, column = 3)
 
 button0 = tk.Button(root, text = "  0  ", command = c0)
-button = tk.Button(root, text = "  ,  ", command = c)
+dot_button = tk.Button(root, text = "  ,  ", command = dot)
 button0.grid(row = 4, column = 0)
-button.grid(row = 4, column = 1)
+dot_button.grid(row = 4, column = 1)
 
+# Creating label with text on 1. row
 label = tk.Label(root, text = "Výsledok je v termináli")
 label.grid(row = 0, columnspan = 3)
 
